6f879c6caadc3488fdf57ec8b67ede8e4e29bd46
rikarumi <rika.rumiyati@gmail.com>
2017-08-12 17:20:17 +0700

    add new post

---
+++ _posts/2017-08-12-short-summary-on-a-book-titled-cooked.m
@@ -0,0 +1,17 @@
+---
+layout: post
+title: "A book summary: Cooked"
+date: 2015-09-12 11:38:00
+image: '/assets/img/'
+description: 'This is key points slide that I made after reading a book titled Cooked by Michael Pollan'
+tags:
+- book
+- review
+- cooking
+- history
+categories:
+- Intro
+twitter_text: 'Cooked'
+---
+
+<iframe src="https://drive.google.com/file/d/0B_ELLCkugV9sWjRMSkJ4N0kxMTQ/preview" width="100%" height="100%"></iframe>
